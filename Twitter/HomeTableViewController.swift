7d020a4ae90da5b27d933bebb554da219faa47e0
Kelley Neubauer <kelleyneubauer@gmail.com>
2020-10-10 17:26:03 -0700

    Computer is freaking out. Backing up work

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -110,6 +110,8 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        
         
         return cell
     }
